9ec0d39d5039a4783dee132af749743a5797009e
MonkeyManCpp <sum8tkumar39699@gmail.com>
2025-09-01 05:17:40 +0530

    signup backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,37 @@
-export const signup = (req, res) => {
-  res.send("signup route");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    // now hashing password
+    // eg: 123456 -> (has hash accordingly)
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already Exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      passowrd: hashPassword,
+    });
+
+    if(newUser){
+      // generate jwt token here
+      
+    }else{
+      res.status(400).json({message:"Invalid User data"});
+    }
+
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,10 @@ import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
+
 const PORT = process.env.PORT 
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
